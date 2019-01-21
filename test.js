4cd27a415bf924961261b46c73deda0e6a2ae87b
vsrathore <vishan.singh23@gmail.com>
2019-01-21 14:22:25 +0530

    Other function

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-function findChar(tmpStr, fndChr){
-    return tmpStr.includes(fndChr)
+function webconnect(urlX){
+  var response = UrlFetchApp.fetch(urlX, {muteHttpExceptions: true})
+  if(response.getResponseCode()==200){ return response.getContentText() }
+  else{ return false }
+}
+
+function spliter(data, start, end){
+  var tempData = data.split(start)[1]
+  return tempData.split(end)[0]
 }
